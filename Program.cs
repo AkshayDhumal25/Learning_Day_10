cd38ac2e219bf02f2f5b8687529570426f0f6186
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-28 09:21:41 +0530

    Generics

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Generics
+using System;
+namespace generics
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enterthe 1st Element : ");
+            var a = Console.ReadLine();
+            Console.WriteLine("Enter the 2nd Element : ");
+            var b = Console.ReadLine();
+            swap(a, b);
+            Console.WriteLine($"A: {a} and B: {b}");
+        }
+        static void swap<T>(T a, T b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
